1b6cfdf2a066e79a8febe48bb34eaafa8df8f8d0
ruanvalente <ruanopensuselinux@gmail.com>
2022-07-29 18:44:31 -0300

    feat: add table course list

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from '../model/course';
+
+import { v4 as uuid } from 'uuid';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  courses: Course[] = [
+    {
+      _id: uuid(),
+      name: 'Curso de Angular 2+',
+      category: 'frontend',
+    },
+    {
+      _id: uuid(),
+      name: 'Curso de Angular 2+',
+      category: 'frontend',
+    },
+    {
+      _id: uuid(),
+      name: 'Curso de Angular 2+',
+      category: 'frontend',
+    },
+    {
+      _id: uuid(),
+      name: 'Curso de Angular 2+',
+      category: 'frontend',
+    },
+    {
+      _id: uuid(),
+      name: 'Curso de Angular 2+',
+      category: 'frontend',
+    },
+    {
+      _id: uuid(),
+      name: 'Curso de Angular 2+',
+      category: 'frontend',
+    },
+    {
+      _id: uuid(),
+      name: 'Curso de Angular 2+',
+      category: 'frontend',
+    },
+    {
+      _id: uuid(),
+      name: 'Curso de Angular 2+',
+      category: 'frontend',
+    },
+    {
+      _id: uuid(),
+      name: 'Curso de Angular 2+',
+      category: 'frontend',
+    },
+  ];
+  displayedColumns = ['name', 'category'];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

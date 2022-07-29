39478debc4931e88bb3944dd893355d97c014af3
ruanvalente <ruanopensuselinux@gmail.com>
2022-07-29 18:46:01 -0300

    refactor: create a module for import's angular material modules

--- frontend/src/app/courses/courses.module.ts
+++ frontend/src/app/courses/courses.module.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MaterialModule } from '../shared/material/material.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
-  declarations: [
-    CoursesComponent
-  ],
-  imports: [CommonModule, CoursesRoutingModule],
+  declarations: [CoursesComponent],
+  imports: [CommonModule, CoursesRoutingModule, MaterialModule],
 })
 export class CoursesModule {}
